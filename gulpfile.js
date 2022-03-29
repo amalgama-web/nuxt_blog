227efe32668e574d0d8d4765908354f1d3f5da65
amalgama <amalgama43@mail.ru>
2022-03-29 20:45:51 +0300

    svg icons

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var svgSprite = require('gulp-svg-sprite');
+
+gulp.task('svg-sprite', function (cb) {
+    return gulp.src('/assets/img/svg-separate/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                symbol: true
+            }
+        }))
+        .pipe(gulp.dest('assets/img/sprites/'));
+});
+
+
+
+
